64fea88043405ae28c4841b6281f54b72a8005de
beckham <beckham.lam@mail.mcgill.ca>
2020-09-05 22:40:35 -0400

    more data in landing page

--- markplace-app/src/app/app.module.ts
+++ markplace-app/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialCommonModule } from '../app/material/material.module';
 import { CommonComponentModule }  from './components/common/common.module';
 import { HomeCommonModule } from './components/home/home.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [
     AppComponent
@@ -17,7 +18,8 @@ import { HomeCommonModule } from './components/home/home.module';
     BrowserAnimationsModule,
     MaterialCommonModule,
     CommonComponentModule,
-    HomeCommonModule
+    HomeCommonModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- markplace-app/src/app/components/home/landing-page/landing-page.component.ts
+++ markplace-app/src/app/components/home/landing-page/landing-page.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  LandingCardConfig,
+  LandingCardPosition,
+} from '../landing-page-card/landing-page-card.model';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
 })
 export class LandingPageComponent implements OnInit {
+  landingCards: LandingCardConfig[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.landingCards = [
+      {
+        textPosition: LandingCardPosition.left,
+        imagePosition: LandingCardPosition.right,
+        textValue:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lectus ante, euismod pulvinar finibus nec, eleifend in nulla. Suspendisse eu.',
+        imageValue: '/assets/images/planning.svg',
+      },
+      {
+        textPosition: LandingCardPosition.right,
+        imagePosition: LandingCardPosition.left,
+        textValue:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lectus ante, euismod pulvinar finibus nec, eleifend in nulla. Suspendisse eu.',
+        imageValue: '/assets/images/growth.svg',
+      },
+      {
+        textPosition: LandingCardPosition.left,
+        imagePosition: LandingCardPosition.right,
+        textValue:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse commodo turpis in scelerisque tristique. Morbi pretium elit quis egestas cursus. Pellentesque in nisl non nulla aliquet congue at.',
+        imageValue: '/assets/images/security.svg',
+      },
+    ];
   }
-
 }
