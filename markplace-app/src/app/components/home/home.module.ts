7331c1fcf6bb04a13edcf070dc7b27cc664f5113
beckham <beckham.lam@mail.mcgill.ca>
2020-10-10 23:02:32 -0400

    landing styling

--- markplace-app/src/app/components/home/home.module.ts
+++ markplace-app/src/app/components/home/home.module.ts
@@ -4,10 +4,15 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LandingPageCardComponent } from './landing-page-card/landing-page-card.component';
 import { CommonComponentModule } from '../common/common.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialCommonModule } from 'src/app/material/material.module';
 @NgModule({
-    imports: [CommonModule, FlexLayoutModule, CommonComponentModule],
-    exports: [LandingPageComponent,LandingPageCardComponent],
-    declarations: [LandingPageComponent,LandingPageCardComponent],
-  })
-  export class HomeCommonModule {}
-  
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    CommonComponentModule,
+    MaterialCommonModule,
+  ],
+  exports: [LandingPageComponent, LandingPageCardComponent],
+  declarations: [LandingPageComponent, LandingPageCardComponent],
+})
+export class HomeCommonModule {}
