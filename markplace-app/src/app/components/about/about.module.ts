ae296746985026a4091bf4ce27cd7344c8694168
beckham <beckham.lam@mail.mcgill.ca>
2020-10-31 19:59:14 -0400

    about us page, layout

--- markplace-app/src/app/components/about/about.module.ts
+++ markplace-app/src/app/components/about/about.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { CommonComponentModule } from '../common/common.module';
 @NgModule({
-  imports: [CommonModule, FlexLayoutModule],
+  imports: [CommonModule, FlexLayoutModule, CommonComponentModule],
   exports: [AboutComponent],
   declarations: [AboutComponent],
 })
