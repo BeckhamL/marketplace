b6386ce9acc10927b6afc27d2414739b3b81331c
beckham <beckham.lam@mail.mcgill.ca>
2020-12-02 14:53:38 -0500

    mongo env

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 const mongoose = require('mongoose');
-const url = 'mongodb://127.0.0.1:27017/marketplace';
+const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/marketplace';
 
 app.use(express.json());
 app.use(express.static(__dirname + "/dist/markplace-app"));
