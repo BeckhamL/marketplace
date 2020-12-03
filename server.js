9ce7839a24bd400e81bcf17e3a97f9ded682c00a
beckham <beckham.lam@mail.mcgill.ca>
2020-12-03 16:25:57 -0500

    heroku test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const path = require("path");
 const express = require("express");
 const server = express();
-
+const MongoClient = require("mongodb").MongoClient;
 const body_parser = require("body-parser");
 
 // parse JSON (application/json content-type)
@@ -11,27 +11,46 @@ server.use(body_parser.json());
 server.use(express.json());
 server.use(cors());
 
-const db = require("./db");
+//const db = require("./db");
+const dbConnectionUrl = process.env.MONGO_URL;
 const dbName = process.env.dbName;
 const collectionName = process.env.collectionName;
 
-// db.initialize(
-//   dbName,
-//   collectionName,
-//   function (dbCollection) {
-//     server.get("/workshops/:id", (request, response) => {
-//       const itemId = request.params.id;
+function initialize(
+  dbName,
+  dbCollectionName,
+  successCallback
+) {
+  MongoClient.connect(dbConnectionUrl, function (err, dbInstance) {
+    if (err) {
+      console.log(`[MongoDB connection] ERROR: ${err}`);
+    } else {
+      const dbObject = dbInstance.db(dbName);
+      const dbCollection = dbObject.collection(dbCollectionName);
+      console.log("[MongoDB connection] SUCCESS");
 
-//       dbCollection.findOne({ _id: itemId }, (error, result) => {
-//         if (error) throw error;
-//         response.json(result);
-//       });
-//     });
-//   },
-//   function (err) {
-//     throw err;
-//   }
-// );
+      successCallback(dbCollection);
+    }
+  });
+}
+
+initialize(
+  dbName,
+  collectionName,
+  function (dbCollection) {
+    server.get("/workshops/:id", (request, response) => {
+      const itemId = request.params.id;
+
+      dbCollection.findOne({ _id: itemId }, (error, result) => {
+        if (error) throw error;
+        response.json(result);
+      });
+    });
+  },
+  function (err) {
+    throw err;
+  }
+);
 
 server.use(express.static(__dirname + "/dist/markplace-app"));
 
