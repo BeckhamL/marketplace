a046b4129836e10bed13fda817d7ddcb1506a176
beckham <beckham.lam@mail.mcgill.ca>
2020-12-05 14:10:57 -0500

    server.js change

--- server.js
+++ server.js
@@ -20,14 +20,14 @@ mongoose.connection.on("connected", () => {});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/api", routes);
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "/dist/markplace-app"));
 
-  app.get("/*", function (req, res) {
+  app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname + "/dist/markplace-app/index.html"));
   });
 }
-
-app.use("/api", routes);
 
 app.listen(PORT, console.log(`Server is starting at ${PORT}`));
