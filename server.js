46e4b4bccf7879df7c830cf5f328a04ee95d728f
beckham <beckham.lam@mail.mcgill.ca>
2020-12-07 15:17:35 -0500

    fix server.js typo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 const workshops = require("./routes/workshops");
 const courses = require("./routes/courses");
-const blogs = requrire("./routes/blogs");
+const blogs = require("./routes/blogs");
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
